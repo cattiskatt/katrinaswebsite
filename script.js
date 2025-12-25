8788c843563b24ee89366d4e20c5fa6260168266
Katrina Lin <katrinalin1244@gmail.com>
2025-12-24 22:37:26 -0600

    sumtingwong

---
+++ script.js
@@ -0,0 +1,14 @@
+function openModal() {
+  document.getElementById("projectModal").style.display = "block";
+}
+
+function closeModal() {
+  document.getElementById("projectModal").style.display = "none";
+}
+
+window.onclick = function (event) {
+  const modal = document.getElementById("projectModal");
+  if (event.target === modal) {
+    modal.style.display = "none";
+  }
+};
