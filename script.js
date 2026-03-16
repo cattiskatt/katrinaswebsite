3e84b825335365bba32d3f3c27ac158083c23068
katrinal-ux <klin45@illinois.edu>
2026-03-15 20:00:28 -0500

    REVAMPED THE WEBSITE

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
 function openModal() {
   document.getElementById("projectModal").style.display = "block";
 }
+function openModal2() {
+  document.getElementById("projectModal2").style.display = "block";
+}
 
 function closeModal() {
   document.getElementById("projectModal").style.display = "none";
+  document.getElementById("projectModal2").style.display = "none";
 }
 
 window.onclick = function (event) {
   const modal = document.getElementById("projectModal");
+  const modal2 = document.getElementById("projectModal2");
   if (event.target === modal) {
     modal.style.display = "none";
+  }
+  if (event.target === modal2) {
+    modal2.style.display = "none";
   }
 };
